cb8f131e253ee37e7b1412ee3cd2363cdfb9075e
magezatovar <gumedeowami91@gmail.com>
2025-05-17 15:20:13 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Mageza2.Obot",
-  botOwner: 'Owami', // Your name
-  ownerNumber: '717873011', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: '~Owami~ ❤️‍🩹🔥', // Your name
+  ownerNumber: '27717873011', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
